0c26b53e011b88917229c18b5bdd94fec3ce5e48
Lake Hu <hudasha@scu.edu.cn>
2022-10-14 16:49:55 +0800

    评价指标MSE，RMSE，MAE

--- mask_rcnn_tool.py
+++ mask_rcnn_tool.py
@@ -720,6 +720,9 @@ def display_instances(image, boxes, masks, class_ids, class_names, result_path,
             v1 = v[:, [1,0]]
             coef = np.polyfit(v1[:,0], v1[:, 1], 2)
             x_fit = np.polyval(coef, v1[:, 0])
+            # MSE = np.linalg.norm(v1-v, ord=2)**2/len(v)
+            # RMSE = np.linalg.norm(v1-v, ord=2)/len(v)**0.5
+            # MAE = np.linalg.norm(v1-v, ord=1)/len(v)
             fx = np.poly1d(coef)
             dfx = fx.deriv()   # 一阶导
             ddfx = dfx.deriv() # 二阶导
