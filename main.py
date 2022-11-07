e5dc4b24626c7ccf8300e77e02a8579a3b20a265
Lake Hu <hudasha@scu.edu.cn>
2022-11-07 11:32:55 +0800

    move to parent dir

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from messager import fetch_request, push_response
 
 
 if __name__ == '__main__':
+    init_mask_rcnn()   # 按照缺省参数初始化，不在循环内
     while True:
         image = fetch_request()
         if image is not None:
+            test_image(image)  # 分割图像
+            # 结果需要放回Redis
             cv2.imshow('py', image)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
